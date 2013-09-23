8e82bb538fb133806669c0eeec19ca39b00a13cc
iamhaix <iamhaix@gmail.com>
2013-09-23 21:23:12 +0800

    modify test doc ===================== test all functions in base.js

--- frontend-test/jasmine-standalone-1.3.1/spec/BaseSpec.js
+++ frontend-test/jasmine-standalone-1.3.1/spec/BaseSpec.js
@@ -1,13 +1,29 @@
 describe("base.js的测试", function(){
 	
-	it("ceshi",function(){
-		var a = 12;
-		expect(a).toEqual(12);
-	});
-
 	it("include方法，应该能正确引入javascript文件",function(){
 		var scripts = base.include("scripts/b.js");
 		expect(scripts).toMatch(/<script type="text\/javascript" src="scripts\/b.js"><\/script>/);
 	});
 
+	//	var si = base.setCookies('uname','haix',1);
+	/*it("setCookie方法用来添加cookie",function(){
+	
+		var s = base.setCookie({name:'uname',value:'haix'});
+		expect(s).toBeTruthy();
+
+	});*/
+
+	it("setCookie方法用来添加cookie",function(){
+
+		var s = base.setCookie({name:'uname',value:'haix',path:'/'});
+		expect(s).toBeTruthy();
+
+	});
+	
+
+	it("getCookie方法用来获取cookie", function(){
+	
+		var s = base.getCookie('uname');
+		expect(s).toMatch(/haix/);
+	});
 });
